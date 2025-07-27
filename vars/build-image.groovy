5d00b34489b10c2c58be7fbe81669144e8c3247c
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-28 02:54:15 +0300

    Update build-image.groovy

--- vars/build-image.groovy
+++ vars/build-image.groovy
@@ -1,9 +1,15 @@
-def call(String imageName, String credentialsId, String image_environment) {
-    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        sh """
-            docker build -t ${imageName}:${image_environment}${BUILD_NUMBER} .
-            echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-            docker push ${imageName}:${image_environment}${BUILD_NUMBER}
-        """
-    }
+def call(String imageName, String awsRegion, String ecrRepoUri) {
+    sh """
+        # Authenticate Docker with ECR using instance profile
+        aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${ecrRepoUri}
+
+        # Build the Docker image
+        docker build -t ${imageName}:${BUILD_NUMBER} .
+
+        # Tag it with ECR URI
+        docker tag ${imageName}:${BUILD_NUMBER} ${ecrRepoUri}:${BUILD_NUMBER}
+
+        # Push to ECR
+        docker push ${ecrRepoUri}:${BUILD_NUMBER}
+    """
 }
