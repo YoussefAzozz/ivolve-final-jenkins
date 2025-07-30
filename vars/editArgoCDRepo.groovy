559886d473101991cfb9c15911abbd2f049697a4
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-31 01:35:26 +0300

    Update editArgoCDRepo.groovy

--- vars/editArgoCDRepo.groovy
+++ vars/editArgoCDRepo.groovy
@@ -18,14 +18,19 @@ def call() {
         // Commit and push
         dir('argocd-repo') {
             withCredentials([usernamePassword(credentialsId: 'github-creds', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
-        sh '''
-            git config user.email "jenkins@example.com"
-            git config user.name "Jenkins CI"
-            git add .
-            git diff --quiet && echo "No changes to commit" || git commit -m "Update image tag to ${BUILD_NUMBER} and replicas to 4"
-            git push https://${GIT_USER}:${GIT_PASS}@github.com/YoussefAzozz/argocd-ivolve-final.git HEAD:main
-        '''
-    }
-}
+                sh """
+                    git config user.email "jenkins@example.com"
+                    git config user.name "Jenkins CI"
+                    git add .
+
+                    if git diff --quiet; then
+                        echo "No changes to commit"
+                    else
+                        git commit -m "Update image tag to ${BUILD_NUMBER} and replicas to 4"
+                        git push https://${GIT_USER}:${GIT_PASS}@github.com/YoussefAzozz/argocd-ivolve-final.git HEAD:main
+                    fi
+                """
+            }
+        }
     }
 }
