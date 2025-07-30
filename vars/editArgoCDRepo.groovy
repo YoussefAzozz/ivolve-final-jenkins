7ed66d90c473e5ba8b670b2bfa23df4e98e975e4
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-31 00:58:59 +0300

    Update editArgoCDRepo.groovy

--- vars/editArgoCDRepo.groovy
+++ vars/editArgoCDRepo.groovy
@@ -1,10 +1,32 @@
-def call(String image_environment){
-  withCredentials([usernamePassword(credentialsId: '743c416b-fd33-4994-be23-6c1599d6e213', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
-    sh '''
-    git clone https://${GIT_USER}:${GIT_PASS}@github.com/YoussefAzozz/argocd-lab.git
-    cd argocd-lab
-    sed -i "s|image:.*|image: '$DOCKER_IMAGE:${image_environment}${BUILD_NUMBER}'|g" deployment.yml
-    cat deployment.yml
-    '''
-}
+def editArgoCDRepo() {
+    stage('Edit ArgoCD Repo') {
+        // Clone your repo
+        sh '''
+            rm -rf argocd-repo
+            git clone https://github.com/YoussefAzozz/argocd-ivolve-final.git argocd-repo
+        '''
+
+        // Update replicas in patch-deployment.yml
+        sh '''
+            sed -i 's/replicas: [0-9]\\+/replicas: 4/' argocd-repo/overlays/prod/patch-deployment.yml
+        '''
+
+        // Update image tag to Jenkins BUILD_NUMBER in kustomization.yml
+        sh """
+            sed -i 's/newTag: .*/newTag: "${BUILD_NUMBER}"/' argocd-repo/overlays/prod/kustomization.yml
+        """
+
+        // Commit and push the changes
+        dir('argocd-repo') {
+            withCredentials([usernamePassword(credentialsId: 'github-creds', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
+                sh '''
+                    git config user.email "jenkins@example.com"
+                    git config user.name "Jenkins CI"
+                    git add .
+                    git commit -m "Update image tag to ${BUILD_NUMBER} and replicas to 4"
+                    git push https://${GIT_USER}:${GIT_PASS}@github.com/YoussefAzozz/argocd-ivolve-final.git HEAD:main
+                '''
+            }
+        }
+    }
 }
