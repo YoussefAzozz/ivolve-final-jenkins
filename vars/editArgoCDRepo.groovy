09fdee19f10d5c9c93ffce66001dc37bba9fcc85
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-31 02:03:44 +0300

    Update editArgoCDRepo.groovy

--- vars/editArgoCDRepo.groovy
+++ vars/editArgoCDRepo.groovy
@@ -15,9 +15,26 @@ def call() {
             sed -i 's/newTag: .*/newTag: "${BUILD_NUMBER}"/' argocd-repo/overlays/prod/kustomization.yml
         """
 
+        def call() {
+    script {
+        sh '''
+            rm -rf argocd-repo
+            git clone https://github.com/YoussefAzozz/argocd-ivolve-final.git argocd-repo
+        '''
+
+        // Update replicas
+        sh '''
+            sed -i 's/replicas: [0-9]\\+/replicas: 4/' argocd-repo/overlays/prod/patch-deployment.yml
+        '''
+
+        // Update image tag
+        sh """
+            sed -i 's/newTag: .*/newTag: "${BUILD_NUMBER}"/' argocd-repo/overlays/prod/kustomization.yml
+        """
+
         dir('argocd-repo') {
             withCredentials([usernamePassword(credentialsId: 'git-creds', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
-                sh '''#!/bin/bash
+                sh """#!/bin/bash
                     set -e
                     git config user.email "jenkins@example.com"
                     git config user.name "Jenkins CI"
@@ -30,8 +47,11 @@ def call() {
                         git commit -m "Update image tag to ${BUILD_NUMBER} and replicas to 4"
                         git push https://${GIT_USER}:${GIT_PASS}@github.com/YoussefAzozz/argocd-ivolve-final.git HEAD:master
                     fi
-                '''
+                """
             }
         }
+    }
+}
+
     }
 }
