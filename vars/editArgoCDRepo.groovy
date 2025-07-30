92d1bc3a6721c8c6460d08fb4c62c5058b2f230d
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-31 02:14:30 +0300

    Update editArgoCDRepo.groovy

--- vars/editArgoCDRepo.groovy
+++ vars/editArgoCDRepo.groovy
@@ -18,21 +18,23 @@ def call() {
 
 dir('argocd-repo') {
     withCredentials([usernamePassword(credentialsId: 'git-creds', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
-        sh '''#!/bin/bash
-            set -e
-            git config user.email "jenkins@example.com"
-            git config user.name "Jenkins CI"
+    sh '''#!/bin/bash
+        set -e
+        git config user.email "jenkins@example.com"
+        git config user.name "Jenkins CI"
 
-            git add overlays/prod/patch-deployment.yml overlays/prod/kustomization.yml
+        git add overlays/prod/patch-deployment.yml overlays/prod/kustomization.yml
 
-            if git diff --cached --quiet; then
-                echo "No changes to commit"
-            else
-                git commit -m "Update image tag to $BUILD_NUMBER and replicas to 4"
-                git push https://$GIT_USER:$GIT_PASS@github.com/YoussefAzozz/argocd-ivolve-final.git HEAD:master
-            fi
-        '''
-    }
+        if git diff --cached --quiet; then
+            echo "No changes to commit"
+        else
+            ENCODED_PASS=$(python3 -c "import urllib.parse,os; print(urllib.parse.quote(os.environ['GIT_PASS']))")
+            git commit -m "Update image tag to $BUILD_NUMBER and replicas to 4"
+            git push https://$GIT_USER:$ENCODED_PASS@github.com/YoussefAzozz/argocd-ivolve-final.git HEAD:master
+        fi
+    '''
+}
+
 }
 
     }
