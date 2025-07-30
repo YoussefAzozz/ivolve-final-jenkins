fcf1298e628c713bd4872bf7fe2f2356f1692044
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-31 02:09:33 +0300

    Update editArgoCDRepo.groovy

--- vars/editArgoCDRepo.groovy
+++ vars/editArgoCDRepo.groovy
@@ -16,23 +16,24 @@ def call() {
             sed -i 's/newTag: .*/newTag: "${BUILD_NUMBER}"/' argocd-repo/overlays/prod/kustomization.yml
         """
 
-        dir('argocd-repo') {
-            withCredentials([usernamePassword(credentialsId: 'git-creds', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
-                sh """#!/bin/bash
-                    set -e
-                    git config user.email "jenkins@example.com"
-                    git config user.name "Jenkins CI"
+dir('argocd-repo') {
+    withCredentials([usernamePassword(credentialsId: 'git-creds', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
+        sh '''#!/bin/bash
+            set -e
+            git config user.email "jenkins@example.com"
+            git config user.name "Jenkins CI"
 
-                    git add overlays/prod/patch-deployment.yml overlays/prod/kustomization.yml
+            git add overlays/prod/patch-deployment.yml overlays/prod/kustomization.yml
 
-                    if git diff --cached --quiet; then
-                        echo "No changes to commit"
-                    else
-                        git commit -m "Update image tag to ${BUILD_NUMBER} and replicas to 4"
-                        git push https://${GIT_USER}:${GIT_PASS}@github.com/YoussefAzozz/argocd-ivolve-final.git HEAD:master
-                    fi
-                """
-            }
-        }
+            if git diff --cached --quiet; then
+                echo "No changes to commit"
+            else
+                git commit -m "Update image tag to $BUILD_NUMBER and replicas to 4"
+                git push https://$GIT_USER:$GIT_PASS@github.com/YoussefAzozz/argocd-ivolve-final.git HEAD:master
+            fi
+        '''
+    }
+}
+
     }
 }
